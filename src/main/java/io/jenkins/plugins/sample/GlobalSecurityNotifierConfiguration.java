86cc4b2055c5805ec3f085adc15530d46ad84901
Yaroslav Afenkin <yaroslav.afenkin@gmail.com>
2021-10-28 10:30:10 +0300

    WIP

--- src/main/java/io/jenkins/plugins/sample/GlobalSecurityNotifierConfiguration.java
+++ src/main/java/io/jenkins/plugins/sample/GlobalSecurityNotifierConfiguration.java
@@ -1,7 +1,9 @@
 package io.jenkins.plugins.sample;
 
+import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.Extension;
-import hudson.model.PersistentDescriptor;
+import hudson.model.Descriptor;
+import hudson.model.RootAction;
 import hudson.util.DescribableList;
 import jenkins.model.GlobalConfiguration;
 import net.sf.json.JSONObject;
@@ -12,8 +14,8 @@ import java.io.IOException;
 import java.util.Collections;
 
 @Extension
-@Symbol("securityNotifiers")
-public class GlobalSecurityNotifierConfiguration extends GlobalConfiguration implements PersistentDescriptor {
+@Symbol("configureSecurityEvents")
+public class GlobalSecurityNotifierConfiguration extends GlobalConfiguration implements RootAction {
 
     private final DescribableList<SecurityNotifierStrategy, SecurityNotifierStrategyDescriptor> securityNotifiers =
             new DescribableList<>(this, Collections.singletonList(new LoggingSecurityNotifierStrategyImpl()));
@@ -27,12 +29,30 @@ public class GlobalSecurityNotifierConfiguration extends GlobalConfiguration imp
         return this.securityNotifiers;
     }
 
-    public void setSecurityNotifiers(StaplerRequest req, JSONObject json) throws FormException {
+    @Override
+    public boolean configure(StaplerRequest req, JSONObject json) throws Descriptor.FormException {
         try {
             this.securityNotifiers.rebuildHetero(req, json, SecurityNotifierStrategyDescriptor.all(), "securityNotifiers");
+            return true;
         } catch (IOException ex) {
             throw new FormException(ex, "securityNotifiers");
         }
+    }
+
+    @Override
+    public String getIconFileName() {
+        return null;
+    }
+
+    @Override
+    public String getUrlName() {
+        return "configureSecurityEvents";
+    }
+
+    @NonNull
+    @Override
+    public String getDisplayName() {
+        return "Security notifications configuration";
     }
 
     public static GlobalSecurityNotifierConfiguration get() {
--- src/main/java/io/jenkins/plugins/sample/SecurityNotifierImpl.java
+++ src/main/java/io/jenkins/plugins/sample/SecurityNotifierImpl.java
@@ -10,36 +10,36 @@ public class SecurityNotifierImpl extends SecurityListener {
 
     @Override
     protected void authenticated2(@NonNull UserDetails details) {
-//        notify2(details.getUsername() + " has authenticated.");
+        this.notify2(details.getUsername() + " has authenticated.");
     }
 
     @Override
     protected void failedToAuthenticate(@NonNull String username) {
-//        notify2(username + " has failed to authenticate.");
+        this.notify2(username + " has failed to authenticate.");
     }
 
     @Override
     protected void loggedIn(@NonNull String username) {
-//        notify2(username + " has logged in.");
+        this.notify2(username + " has logged in.");
     }
 
     @Override
     protected void userCreated(@NonNull String username) {
-//        notify2(username + " was created.");
+        this.notify2(username + " was created.");
     }
 
     @Override
     protected void failedToLogIn(@NonNull String username) {
-//        notify2(username + " has failed to log in.");
+        this.notify2(username + " has failed to log in.");
     }
 
     @Override
     protected void loggedOut(@NonNull String username) {
-//        notify2(username + " has logged out.");
+        this.notify2(username + " has logged out.");
     }
 
-//    private void notify2(String message) {
-//        SecurityNotifierConfiguration.get().getSecurityNotifiers()
-//                .forEach(securityNotifierStrategy -> securityNotifierStrategy.sendNotification(message));
-//    }
+    private void notify2(String message) {
+        GlobalSecurityNotifierConfiguration.get().getSecurityNotifiers()
+                .forEach(securityNotifierStrategy -> securityNotifierStrategy.sendNotification(message));
+    }
 }
